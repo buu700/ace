48302f3ea52f98e2c4289589c476b8fcf86b86ea
notsyncing <song.fc@gmail.com>
2017-09-29 10:14:57 +0800

    Create a default Application class for Android after prepare.

---
+++ src/plugin_hooks/android_after_prepare.js
@@ -0,0 +1,49 @@
+var fs = require('fs');
+var fsextra = require('fs-extra');
+var path = require('path');
+
+function createApplicationClass(context) {
+    var configXml = path.join(context.opts.projectRoot, 'config.xml');
+    var parser = context.requireCordovaModule('elementtree');
+
+    var configData = fs.readFileSync(configXml).toString();
+    var configs = parser.parse(configData);
+
+    var packageName = configs.getroot().attrib.id;
+
+    var androidSrcRootPath = path.join(context.opts.projectRoot, "native/android/src");
+    var pkgPath = path.join(androidSrcRootPath, packageName.replace(/\./g, "/"));
+    fsextra.ensureDirSync(pkgPath);
+
+    var appName = packageName.replace(/\./g, "_") + "_Application";
+
+    var pluginSrcRootPath = path.join(__dirname, "..");
+    var androidApplicationClassSrcPath = path.join(pluginSrcRootPath, "android/build/CustomApplication.java");
+    var androidApplicationClassDstPath = path.join(pkgPath, appName + ".java");
+
+    var data = fs.readFileSync(androidApplicationClassSrcPath).toString();
+
+    data = data.replace(/\${PACKAGE_NAME}/g, packageName)
+        .replace(/\${APP_NAME}/g, appName);
+
+    fs.writeFileSync(androidApplicationClassDstPath, data);
+
+    var manifestXml = path.join(context.opts.projectRoot, "platforms/android/AndroidManifest.xml");
+    var manifestData = fs.readFileSync(manifestXml).toString();
+    var manifest = parser.parse(manifestData);
+
+    var appNode = manifest.getroot().findall("./application")[0];
+
+    if (!appNode) {
+        throw new Error("No application tag found in manifest!");
+    }
+
+    appNode.set("android:name", packageName + "." + appName);
+
+    var newManifest = manifest.write();
+    fs.writeFileSync(manifestXml, newManifest);
+}
+
+module.exports = function (context) {
+    createApplicationClass(context);
+};
